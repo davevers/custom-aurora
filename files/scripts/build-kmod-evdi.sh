69307639bf908567eb39793420bbbde4021662b3
Dave Verstrate <daverstrate@gmail.com>
2025-03-20 08:25:07 +0100

    fix add repo command

--- files/scripts/build-kmod-evdi.sh
+++ files/scripts/build-kmod-evdi.sh
@@ -12,7 +12,7 @@ set -e pipefail
 
 ### BUILD evdi (succeed or fail-fast with debug output)
 export CFLAGS="-fno-pie -no-pie"
-dnf config-manager --add-repo=https://negativo17.org/repos/fedora-nvidia.repo
+dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
 dnf install -y \
   kmod-evdi*.fc"${RELEASE}.${ARCH}" akmod-evdi-*.fc"${RELEASE}.${ARCH}"
 akmods --force --kernels "${KERNEL}" --kmod evdi
